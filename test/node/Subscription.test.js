f5612da93abae7022e68b0727dfc33ade66bd1b5
Artem Artemyev <aartemev@naumen.ru>
2016-01-25 19:09:54 +0500

    initial test of Subscription

---
+++ test/node/Subscription.test.js
@@ -0,0 +1,34 @@
+import Subscription, { * as utils } from '../../lib/Subscription';
+import { Collection, Random } from 'marsdb';
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
+chai.should();
+
+
+describe('Subscription', function () {
+  describe('#_getDocumentsByCursor', function () {
+
+  });
+
+  describe('#_diffDocumentsMap', function () {
+
+  });
+
+  describe('#_handleCursorChange', function () {
+
+  });
+
+  describe('#start', function () {
+
+  });
+
+  describe('#stop', function () {
+
+  });
+
+  describe('#getDocumentsMap', function () {
+
+  });
+});
