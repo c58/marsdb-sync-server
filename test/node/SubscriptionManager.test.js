38a4dee1f896966c6fb31dd591f9a4bb16826617
Artem Artemyev <aartemev@naumen.ru>
2016-01-25 19:12:13 +0500

    added SubscriptionManager tests

---
+++ test/node/SubscriptionManager.test.js
@@ -0,0 +1,54 @@
+import SubscriptionManager, { * as utils } from '../../lib/SubscriptionManager';
+import { Collection, Random } from 'marsdb';
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
+chai.should();
+
+
+describe('SubscriptionManager', function () {
+  describe('#_diffObjects', function () {
+
+  });
+
+  describe('#_diffAddedWithRemote', function () {
+
+  });
+
+  describe('#_diffChangedWithRemote', function () {
+
+  });
+
+  describe('#_diffRemovedWithRemote', function () {
+
+  });
+
+  describe('#publish', function () {
+
+  });
+
+  describe('#whenAllCursorsUpdated', function () {
+
+  });
+
+  describe('#_handleSubscribe', function () {
+
+  });
+
+  describe('#_handleUnsubscribe', function () {
+
+  });
+
+  describe('#_handleSubscriptionUpdate', function () {
+
+  });
+
+  describe('#_appendDocuments', function () {
+
+  });
+
+  describe('#_removeDocuments', function () {
+
+  });
+});
